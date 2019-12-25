c402ed68a590263bb45ab41225afd38b6a315802
Anan Aziz <anan.aziz15@hotmail.com>
2019-12-24 22:21:35 -0700

    Second commit

--- VideoRental/Controllers/MoviesController.cs
+++ VideoRental/Controllers/MoviesController.cs
@@ -3,14 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using VideoRental.Models;
 
 namespace VideoRental.Controllers
 {
     public class MoviesController : Controller
     {
+        //GET: Movies/Random
         public ActionResult Index()
         {
-            return View ();
+            var movie = new Movie() { Name = "Shrek!" };
+            return View (movie);
         }
     }
 }
