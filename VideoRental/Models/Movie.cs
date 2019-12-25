e6af102b2af503e91c2e4729885fe26d9a3f1dde
Anan Aziz <anan.aziz15@hotmail.com>
2019-12-24 22:19:53 -0700

    First commit

---
+++ VideoRental/Models/Movie.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace VideoRental.Models
+{
+    //This class is a plain old clr object- POCO
+    //which represents the state and behavior of our application in
+    //terms of its problem domain.
+    //Does not have any behavior or logic, only has properties.
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+    }
+
+    //This page will be in move/random
+}
